41d2fef6a49dd68efddb24b71a27fcd49e287f82
Lysenko Vladyslav <vlad.lysenko1999@gmail.com>
2023-10-08 18:46:03 +0300

    Add to project new folders and class user

---
+++ Core/Models/User/User.cs
@@ -0,0 +1,28 @@
+﻿namespace WPF_WEATHER_APP_AVGUSTIN.Core.Models.User
+{
+    /// <summary>
+    ///  User model
+    ///  - контейнер для хранения данных о пользователе
+    /// </summary>
+    public class User
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public User(string name, string password)
+        {
+            Name = name;
+            Password = password;
+        }
+
+        public User()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Password}";
+        }
+    }
+}
